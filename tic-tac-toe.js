55e10ab21f0e2803f1cff1c376dcbd5600f2c1b5
Melicia Robinson <Melicia@Melicias-MacBook-Pro.local>
2020-10-09 14:09:43 -0500

    Completed Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,14 @@
+
+window.addEventListener('load', (event) => {
+    document.querySelector('#board').getElementsByTagName('div')[0].setAttribute('class', 'square');
+    document.getElementById('board').getElementsByTagName('div')[0].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[1].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[2].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[3].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[4].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[5].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[6].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[7].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[8].setAttribute('class', 'square');
+	document.getElementById('board').getElementsByTagName('div')[9].setAttribute('class', 'square');
+});
