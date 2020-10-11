02ad30132196bbeca4aa3ebb21dfb88ec2500748
Melicia Robinson <Melicia@Melicias-MacBook-Pro.local>
2020-10-11 16:09:53 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,53 @@
+window.addEventListener("load", myScript);
+function myScript() {
+	document.getElementById('board').getElementsByTagName('div')[0].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[1].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[2].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[3].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[4].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[5].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[6].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[7].classList.add('square');
+	document.getElementById('board').getElementsByTagName('div')[8].classList.add('square');
 
-window.addEventListener('load', (event) => {
-    document.querySelector('#board').getElementsByTagName('div')[0].setAttribute('class', 'square');
-    document.getElementById('board').getElementsByTagName('div')[0].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[1].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[2].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[3].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[4].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[5].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[6].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[7].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[8].setAttribute('class', 'square');
-	document.getElementById('board').getElementsByTagName('div')[9].setAttribute('class', 'square');
-});
+	const squares = document.querySelectorAll('.square');
+	let playing =true;
+	let playerisX = true;
+	let board =[];
+	
+	squares.forEach(square => {
+		square.addEventListener('click', whenclick, {once: true})
+	})
+	function whenclick(e) {
+		if (playerisX == true) {
+			e.target.classList.add("X");
+			e.target.textContent = "X";
+			board.push("X");
+			playerisX = false;
+
+		}
+		else {
+			e.target.classList.add("O");
+			e.target.textContent = "O";
+			board.push("O");
+			playerisX = true;
+		}
+
+	}
+	
+}
+/*let elementsArray = document.querySelectorAll(".square");
+	elementsArray.forEach(function(elem) {
+	    elem.addEventListener('click', whenclick) 
+	});
+function whenclick(e) {
+	var x = e.target
+	x.classList.add('X');
+	x.innerHTML = 'X';
+}*/
+/*document.getElementByClassName("square").addEventListener("click", myScript);
+function myScript() {
+	document.getElementByClassName("square").classList.add('X');
+}*/
+
+
