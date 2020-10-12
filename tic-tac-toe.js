1ac974cfae5ed39911bf03645f9c5ab5b7312c93
Melicia Robinson <Melicia@Melicias-MacBook-Pro.local>
2020-10-11 22:02:24 -0500

    Completed Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,51 +10,82 @@ function myScript() {
 	document.getElementById('board').getElementsByTagName('div')[7].classList.add('square');
 	document.getElementById('board').getElementsByTagName('div')[8].classList.add('square');
 
-	const squares = document.querySelectorAll('.square');
+	const squares = Array.from(document.querySelectorAll('.square'));
 	let playing =true;
 	let playerisX = true;
-	let board =[];
+	let board =['', '', '', '', '', '', '', '', ''];
+	const win_types = [
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]
+	]
 	
 	squares.forEach(square => {
-		square.addEventListener('mouseover', hover);
-		square.addEventListener('mouseout', nohover);
+		square.addEventListener('mouseover', function(e) {
+		    e.target.classList.add('hover');
+		  });
+		square.addEventListener('mouseout', function(e) {
+		    e.target.classList.remove('hover');
+		  });
 		square.addEventListener('click', whenclick, {once: true})
-	})
+	})	
+	
 	function whenclick(e) {
-		if (playerisX == true) {
+		if (playerisX == true && playing == true) {
 			e.target.classList.add("X");
 			e.target.textContent = "X";
-			board.push("X");
+			const sq = (element) => element == e.target;
+			let i = squares.findIndex(sq);
+			board[i] = "X";
+			if (winner()) {
+				console.log("winner");
+				playing = false;
+				if (playerisX == true) {
+					document.getElementById('status').textContent = "Congratulations! X is the Winner!";
+					document.getElementById('status').classList.add("you-won");
+				}
+			}
 			playerisX = false;
 
 		}
-		else {
+		else if (playerisX == false && playing == true){
 			e.target.classList.add("O");
 			e.target.textContent = "O";
-			board.push("O");
+			const sq = (element) => element == e.target;
+			let i = squares.findIndex(sq);
+			board[i] = "O";
+			if (winner()) {
+				console.log("winner");
+				if (playerisX == false) {
+					document.getElementById('status').textContent = "Congratulations! O is the Winner!";
+					document.getElementById('status').classList.add("you-won");
+				}
+			}
 			playerisX = true;
 		}
+	
 
 	}
-	function hover(e) {
-		e.target.classList.add("hover");
-	}
-	function nohover(e) {
-		e.target.classList.remove("hover");
+	function winner(){
+		let roundwon = false;
+	    for (let i = 0; i <= 7; i++) {
+	        const win = win_types[i];
+	        let a = board[win[0]];
+	        let b = board[win[1]];
+	        let c = board[win[2]];
+	        if (a === '' || b === '' || c === '') {
+	            continue;
+	        }
+	        if (a === b && b === c) {
+	        	roundwon = true;
+	            return roundwon;
+	        }
+	    }
 	}
 }
-/*let elementsArray = document.querySelectorAll(".square");
-	elementsArray.forEach(function(elem) {
-	    elem.addEventListener('click', whenclick) 
-	});
-function whenclick(e) {
-	var x = e.target
-	x.classList.add('X');
-	x.innerHTML = 'X';
-}*/
-/*document.getElementByClassName("square").addEventListener("click", myScript);
-function myScript() {
-	document.getElementByClassName("square").classList.add('X');
-}*/
-
 
